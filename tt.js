83e09f8f52f3501c7482f5ad7d9a484f8cc1751d
alan <296688308@qq.com>
2019-07-12 18:37:26 +0800

    提交

--- tt.js
+++ tt.js
@@ -9,24 +9,24 @@ let path = join('upload', '111.zip')
 // zip.pipe(unzip.Extract({path: join(__dirname,'test')})) // 解压到指定目录下
 
 
-fs.createReadStream('upload/归档.zip').pipe(unzip.Extract({ path: 'test' }));
+// fs.createReadStream('upload/src.zip').pipe(unzip.Extract({ path: 'test' }));
 
 
-var JSZip = require("jszip");
-var fs = require("fs");
+// var JSZip = require("jszip");
+// var fs = require("fs");
+//
+// var zip = new JSZip();
+//
+// var file_content = fs.readFileSync('archive/a.txt');
 
-var zip = new JSZip();
 
-var file_content = fs.readFileSync('archive/a.txt');
+// zip.file("a.txt",file_content);
 
-
-zip.file("a.txt",file_content);
-
-var data = fs.readFileSync("archive/img/pic.jpeg");
-zip.file("img/pic.jpeg", data, {base64: true});
-
-var zipfolder = zip.generate({type:"nodebuffer"});
-
-fs.writeFile("jszip.zip", zipfolder, function(err) {
-	if (err) throw err;
-});
+// var data = fs.readFileSync("archive/img/pic.jpeg");
+// zip.file("img/pic.jpeg", data, {base64: true});
+//
+// var zipfolder = zip.generate({type:"nodebuffer"});
+//
+// fs.writeFile("jszip.zip", zipfolder, function(err) {
+// 	if (err) throw err;
+// });
